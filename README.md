d6afc417bc2cb79f4f30807b0c905ab709309d14
Ricardo Alcocer <ralcocer@gmail.com>
2020-05-15 14:19:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PHP MVC Starter
+
+This repo contains a super simple architecture for a PHP website using a MVC paradigm.
---
+++ views/home.php
@@ -0,0 +1,8 @@
+<?php $this->extend('mainlayout') ?>
+<?php $this->block('title',$title) ?>
+
+this is content?
+
+<?php $this->block('cta') ?>
+Welcome to my site!
+<?php $this->endblock() ?>
